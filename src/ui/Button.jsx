c9bff27b4852db0a3affec4be7cb57e329dcdb3e
bungearntun <love2554f@hotmail.com>
2024-07-17 16:07:58 +0700

    updated : styling order

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 export default function Button({ children, disable, to, type }) {
-  const base = `inline-block rounded-full 
+  const base = `inline-block rounded-full text-sm
   bg-yellow-400 tracking-wide 
   uppercase font-semibold text-stone-800
   hover:bg-yellow-300 transition-colors duration-300
@@ -11,6 +11,11 @@ export default function Button({ children, disable, to, type }) {
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary: "inline-block rounded-full tracking-wide border-2 border-stone-300 text-sm"
+    + " uppercase font-semibold text-stone-400 px-4 py-2.5 md:px-6 md:py-3.5"
+    + " hover:bg-stone-300 transition-colors duration-300 hover:text-stone-800"
+    + " focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-300"
+    + " focus:ring-offset-2 disabled:cursor-not-allowed"
   };
   if (to)
     return (
